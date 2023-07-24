083fb3456bc46b23dab60369e48a594d76bd1fe9
TarunBaskar <117260375+TarunBaskar@users.noreply.github.com>
2023-07-24 11:56:39 +0530

    security

---
+++ security/java/com/example/security/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.neo.security.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.neo.security.entity.User;
+import com.neo.security.repository.UserRepository;
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	
+	public Boolean addUser(User user) {
+		
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		
+		User user1 = User.builder()
+				.Name(user.getName())
+				
+				.email(user.getEmail())
+				.Password(encodedPassword)
+		
+				.build();
+		return repo.save(user1)!=null ? true:false;
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		return repo.findAll();
+	}
+
+	@Override
+	public User getUserByEmail(String email) {
+	
+		return repo.findByEmail(email).get();
+	}
+	
+	
+
+}
